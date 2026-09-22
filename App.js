f27da2d843b7082a42f647fd5a693491fbe04bfb
li2yemxzy <liyemxzy655@gmail.com>
2026-09-22 10:58:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native';
-export default function App() {
+export default function App(){
   return (
-    <View style={{flex:1,backgroundColor:'#0a0a0a',alignItems:'center',justifyContent:'center'}}>
+    <View style={{flex:1,backgroundColor:'#0a0a0a',justifyContent:'center',alignItems:'center',padding:20}}>
       <Text style={{color:'#fff',fontSize:22,fontWeight:'bold'}}>EscrowLock Gold V2</Text>
-      <Text style={{color:'#f5ff00',marginTop:10}}>Build 21 - Fixed</Text>
+      <Text style={{color:'#f5ff00',marginTop:10}}>Build 24 - Final</Text>
+      <Text style={{color:'#aaa',marginTop:20}}>Ready for APK</Text>
     </View>
   );
 }
